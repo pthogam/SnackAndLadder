435c302bca33f8375d49cb143ab199fe73156ccc
pundalik Thogam <pthogam@gmail.com>
2023-03-21 15:20:19 +0530

    file added

---
+++ SnackAndLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackAndLdderproblem
+{
+    public class SnackAndLadder
+    {
+        public static void findstart()
+        {
+            Console.WriteLine("wellcone in snack and Ladder problem");
+            int start = 0;
+            Console.WriteLine("the begging of game is : " +start);
+
+
+
+        }
+            
+    }
+}
